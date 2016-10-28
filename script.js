ee1d3a5fbd6505af13e20278810b4be7cf67009e
sophiayangx <sopyang@nuevaschool.org>
2016-10-28 09:01:33 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ console.log("time to add some firebase code!")
 addListItem("key1" + "/listKey1", "hola!" + "adios!")
 
 var key1 = {
-name:"Sophia"
-age: "15"
-hair: "long"
+  nestedKey1:"hi!"
+  nestedKey2: "bye!"
 }
