aa555e8b971ae3d972eed867071aeb1df26f744c
sophiayangx <sopyang@nuevaschool.org>
2016-10-28 09:06:11 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 var key1 = {
   nestedKey1:"hi!",
   nestedKey2: "bye!",
-  nested: {
-    key8: "b;a;a"
+}
+
+var key2 = {
+  nestedkey1: {
+    nestedkey2: "b;a;a"
   }
 }
 
+
 console.log("time to add some firebase code!")
-storeValue("key1/listKey1", "hola!" + "adios!")
+//storeValue("key1/listKey1", "hola!" + "adios!")
 storeValue("key1",key1)
