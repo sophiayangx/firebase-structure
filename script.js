db5c022a1dd2bf55f5bbbde1e9b958eb84a52a62
sophiayangx <sopyang@nuevaschool.org>
2016-10-28 09:09:08 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 var key1 = {
   nestedKey1:"hi!",
   nestedKey2: "bye!",
+  nested: {
+  
+  }
 }
 
 var key2 = {
   nestedkey1: {
-    nestedkey2: {nestedKey5: notAsNestedKey: "I'm not as nested!"
+    nestedkey2: {nestedKey5: {notAsNestedKey: "I'm not as nested!"}
   
   }
   }
